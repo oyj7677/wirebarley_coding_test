820cd8338d1a14196234cedb7144ef90a0943fa1
oyj <oyj7677@gmail.com>
2022-12-14 23:26:28 +0900

    기본 UI 구성(70%)

--- app/src/main/java/com/example/wirebarley/main/MainFragment.kt
+++ app/src/main/java/com/example/wirebarley/main/MainFragment.kt
@@ -1,10 +1,13 @@
 package com.example.wirebarley.main
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import com.example.wirebarley.R
 
 class MainFragment : Fragment(), MainContract.View {
@@ -16,15 +19,31 @@ class MainFragment : Fragment(), MainContract.View {
 
     private lateinit var presenter: MainContract.Presenter
 
+    private lateinit var sendSpinner: Spinner
+    private lateinit var receiveSpinner: Spinner
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+    @SuppressLint("ResourceType")
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val root = inflater.inflate(R.layout.fragment_main, container, false)
+
+        sendSpinner = root.findViewById(R.id.spinner_send_country)
+        receiveSpinner = root.findViewById(R.id.spinner_receive_country)
+
+        ArrayAdapter.createFromResource(requireContext(), R.array.send_country, android.R.layout.simple_spinner_item )
+            .also { adapter ->
+                sendSpinner.adapter = adapter
+            }
+
+        ArrayAdapter.createFromResource(requireContext(), R.array.receive_country, android.R.layout.simple_spinner_item)
+            .also { adapter ->
+                receiveSpinner.adapter = adapter
+            }
+
+        return root
     }
 
 
